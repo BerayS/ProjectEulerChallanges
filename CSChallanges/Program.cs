b46133338ad0890ed4a30428bb2ba58aec3f2852
Beray <bsezgin00@gmail.com>
2022-09-27 15:05:32 +0300

    Started Challange 4

--- CSChallanges/Program.cs
+++ CSChallanges/Program.cs
@@ -16,5 +16,40 @@
 
 // Third Challange
 // Refference to the class
-Challange3 primeFactors = new Challange3();
-Console.WriteLine(primeFactors.GetPrimeFactors());
+// Challange3 primeFactors = new Challange3();
+// Console.WriteLine(primeFactors.GetPrimeFactors());
+
+List<int> numbers = new List<int>();
+List<int> palindromes = new List<int>();
+
+for (int i = 1; i < 10; i++) {
+    for (int j = 0; j < i; j++) {
+        int a = j * i;
+
+        numbers.Add(a);
+    }
+}
+
+
+Console.WriteLine("Enter a Number : ");
+
+foreach (var item in numbers) {
+    if(item == Reverse(item)) {
+        palindromes.Add(item);
+    }
+}
+
+palindromes.ForEach(Console.WriteLine);
+
+
+int Reverse(int num) {
+    int reverse = 0;
+
+    while (num != 0) {
+        reverse = reverse * 10;
+        reverse = reverse + num % 10;
+        num = num / 10;
+    }
+
+    return reverse;
+}

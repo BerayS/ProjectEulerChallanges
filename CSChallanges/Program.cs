34fc6bdb8140988f84b3afd4921e43c5337f30a0
Beray <bsezgin00@gmail.com>
2022-09-30 17:07:05 +0300

    Just Playing Around

--- CSChallanges/Program.cs
+++ CSChallanges/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using DocumentFormat.OpenXml.Presentation;
+using System.Data.SqlClient;
 //// First Challange
 //// Refference to the class
 //Challange1 firstCode = new Challange1();
@@ -16,5 +17,26 @@
 
 // Third Challange
 // Refference to the class
-Challange3 primeFactors = new Challange3();
-Console.WriteLine(primeFactors.GetPrimeFactors());
+//Challange3 primeFactors = new Challange3();
+//Console.WriteLine(primeFactors.GetPrimeFactors());
+
+		int num;
+		Console.WriteLine("Enter a number :");
+		num = int.Parse(Console.ReadLine());
+
+		if (isPalindrome(num)) {
+			Console.WriteLine("Entered number is a palindrome number");
+		} else {
+			Console.WriteLine("Entered number is not a palindrome number");
+		}
+
+static bool isPalindrome(int num) {
+		int tempValue = num;
+		int reverse = 0;
+		while (tempValue > 0) {
+			reverse = reverse * 10 + tempValue % 10;
+			tempValue = tempValue / 10;
+		}
+
+		return reverse == num;
+	}
